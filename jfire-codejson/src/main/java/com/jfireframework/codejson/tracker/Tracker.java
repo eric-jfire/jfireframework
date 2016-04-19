e4b9319e9cbc73daef5168e6d1133c4b7de6cf05
Administrator <Administrator@WINDOWS-7MB0HRM>
2016-04-19 18:38:07 +0800

    开始对循环引用支持的功能改造

---
+++ jfire-codejson/src/main/java/com/jfireframework/codejson/tracker/Tracker.java
@@ -0,0 +1,47 @@
+package com.jfireframework.codejson.tracker;
+
+import java.util.HashMap;
+
+public class Tracker
+{
+    private HashMap<Object, Pathinfo> map = new HashMap<>();
+    
+    public void put(Object obj, String path)
+    {
+        Pathinfo info = new Pathinfo(obj, path);
+        map.put(obj, info);
+    }
+    
+    public static class Pathinfo
+    {
+        private Object obj;
+        private String path;
+        
+        public Pathinfo(Object obj, String path)
+        {
+            this.obj = obj;
+            this.path = path;
+        }
+        
+        public Object getObj()
+        {
+            return obj;
+        }
+        
+        public void setObj(Object obj)
+        {
+            this.obj = obj;
+        }
+        
+        public String getPath()
+        {
+            return path;
+        }
+        
+        public void setPath(String path)
+        {
+            this.path = path;
+        }
+        
+    }
+}
