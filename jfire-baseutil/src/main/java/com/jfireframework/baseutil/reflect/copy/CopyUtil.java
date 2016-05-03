f6b388c2023cf61422a9f7ef2a87fa9b2d6b3d75
eric <eric@jfire.cn>
2016-05-03 21:14:19 +0800

    为拷贝方法类增加了泛型支持

--- jfire-baseutil/src/main/java/com/jfireframework/baseutil/reflect/copy/CopyUtil.java
+++ jfire-baseutil/src/main/java/com/jfireframework/baseutil/reflect/copy/CopyUtil.java
@@ -1,6 +1,6 @@
 package com.jfireframework.baseutil.reflect.copy;
 
-public interface CopyUtil
+public interface CopyUtil<T, D>
 {
-	public void copy(Object src, Object desc);
+    public void copy(T src, D desc);
 }
