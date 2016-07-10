473f983e4fd7999aa5872728611641045f036ae6
eric <eric@jfire.cn>
2016-07-10 08:53:37 +0800

    增加一个intutil的测试

---
+++ jfire-licp/src/test/java/com/jframework/licp/test/basetest/IntUtilTest.java
@@ -0,0 +1,17 @@
+package com.jframework.licp.test.basetest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import com.jfireframework.licp.util.IntUtil;
+
+public class IntUtilTest
+{
+    @Test
+    public void test()
+    {
+        for (int i =Short.MIN_VALUE; i <= Short.MAX_VALUE; i++)
+        {
+            Assert.assertEquals(i, IntUtil.zag(IntUtil.zig(i)));
+        }
+    }
+}
