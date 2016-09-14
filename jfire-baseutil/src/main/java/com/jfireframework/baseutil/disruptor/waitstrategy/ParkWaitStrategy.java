1e309b9f437a24e58b1a0b79fa8f3528a673a43c
eric <eric@jfire.cn>
2016-09-14 14:24:18 +0800

    增加一段注释

--- jfire-baseutil/src/main/java/com/jfireframework/baseutil/disruptor/waitstrategy/ParkWaitStrategy.java
+++ jfire-baseutil/src/main/java/com/jfireframework/baseutil/disruptor/waitstrategy/ParkWaitStrategy.java
@@ -22,6 +22,12 @@ public class ParkWaitStrategy extends AbstractWaitStrategy
     {
         while (ringArray.isAvailable(next) == false)
         {
+            /**
+             * 这里在cas成功后需要唤醒所有的线程。如果不唤醒会出现一个bug场景导致活锁。
+             * 线程1序号很小，比当前cursor小一个ringsize。线程2序号比当前cursor大1.
+             * 线程1cas失败直接挂起。线程2cas成功后recheck也挂起。生产者由于cursor比线程1的序号大一个ringsize，也无法放入数据。
+             * 导致活锁。
+             */
             if (parkFlag.value() == WORK && parkFlag.compareAndSwap(WORK, PARKED))
             {
                 for (Thread each : threads)
