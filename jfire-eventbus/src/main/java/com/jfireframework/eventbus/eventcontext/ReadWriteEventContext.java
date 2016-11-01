a303dba85b69546eb119778d60618b1c06bab096
eric <eric@jfire.cn>
2016-11-01 19:18:47 +0800

    读写锁开发中

---
+++ jfire-eventbus/src/main/java/com/jfireframework/eventbus/eventcontext/ReadWriteEventContext.java
@@ -0,0 +1,23 @@
+package com.jfireframework.eventbus.eventcontext;
+
+import com.jfireframework.eventbus.event.EventConfig;
+
+public interface ReadWriteEventContext<T extends Enum<? extends EventConfig>> extends EventContext<T>
+{
+    /**
+     * 是否直接执行。初始化的时候都是false
+     * 
+     * @return
+     */
+    public boolean immediateInvoke();
+    
+    public static final int READ  = 1;
+    public static final int WRITE = 2;
+    
+    /**
+     * 1是读取，2是写入
+     * 
+     * @return
+     */
+    public int mode();
+}
--- jfire-eventbus/src/main/java/com/jfireframework/eventbus/executor/ReadWriteExecutor.java
+++ jfire-eventbus/src/main/java/com/jfireframework/eventbus/executor/ReadWriteExecutor.java
@@ -1,26 +1,101 @@
 package com.jfireframework.eventbus.executor;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import com.jfireframework.baseutil.concurrent.MPSCQueue;
+import com.jfireframework.baseutil.exception.UnSupportException;
+import com.jfireframework.baseutil.reflect.ReflectUtil;
 import com.jfireframework.eventbus.bus.EventBus;
-import com.jfireframework.eventbus.event.EventConfig;
-import com.jfireframework.eventbus.event.ParallelLevel;
 import com.jfireframework.eventbus.eventcontext.EventContext;
+import com.jfireframework.eventbus.eventcontext.ReadWriteEventContext;
+import com.jfireframework.eventbus.handler.EventHandler;
+import sun.misc.Unsafe;
 
 public class ReadWriteExecutor implements EventHandlerExecutor
 {
+    static final int SHARED_SHIFT   = 16;
+    static final int SHARED_UNIT    = (1 << SHARED_SHIFT);
+    static final int MAX_COUNT      = (1 << SHARED_SHIFT) - 1;
+    static final int EXCLUSIVE_MASK = (1 << SHARED_SHIFT) - 1;
     
-    private AtomicInteger                    readCount = new AtomicInteger(0);
-    private final MPSCQueue<EventContext<?>> events    = new MPSCQueue<EventContext<?>>();
+    public static int sharedCount(int c)
+    {
+        return c >>> SHARED_SHIFT;
+    }
+    
+    public static int exclusiveCount(int c)
+    {
+        return c & EXCLUSIVE_MASK;
+    }
+    
+    static class ReadWriteLock
+    {
+        private volatile int resourceCount = 0;
+        static final Unsafe  unsafe        = ReflectUtil.getUnsafe();
+        static final long    offset        = ReflectUtil.getFieldOffset("resourceCount", ReadWriteLock.class);
+        
+        public boolean compareAndSet(int expectedValue, int newValue)
+        {
+            return unsafe.compareAndSwapInt(this, offset, expectedValue, newValue);
+        }
+        
+        int count()
+        {
+            return resourceCount;
+        }
+        
+        public int releaseReadLock()
+        {
+            int now = resourceCount;
+            int t = now - SHARED_UNIT;
+            if (compareAndSet(now, t))
+            {
+                return t;
+            }
+            for (;;)
+            {
+                now = resourceCount;
+                t = now - SHARED_UNIT;
+                if (compareAndSet(now, t))
+                {
+                    return t;
+                }
+            }
+        }
+    }
+    
+    private ReadWriteLock                             readWriteLock = new ReadWriteLock();
+    private final MPSCQueue<ReadWriteEventContext<?>> events        = new MPSCQueue<ReadWriteEventContext<?>>();
     
     @Override
     public void handle(EventContext<?> eventContext, EventBus eventBus)
     {
-        Enum<? extends EventConfig> event = eventContext.getEvent();
-        // 不是读取就是写入
-        if (((EventConfig) event).parallelLevel() == ParallelLevel.RW_EVENT_READ)
+        ReadWriteEventContext<?> readWriteEventContext = (ReadWriteEventContext<?>) eventContext;
+        if (readWriteEventContext.mode() == ReadWriteEventContext.READ)
         {
-            
+            if (readWriteEventContext.immediateInvoke() == false)
+            {
+                int now = readWriteLock.resourceCount;
+                if (exclusiveCount(now) != 0)
+                {
+                    events.offer(readWriteEventContext);
+                }
+                else
+                {
+                    int s = sharedCount(now);
+                    if (s == MAX_COUNT)
+                    {
+                        throw new UnSupportException("无法支持这么多锁");
+                    }
+                    if (readWriteLock.compareAndSet(now, now + SHARED_UNIT))
+                    {
+                        _handle(readWriteEventContext, eventBus);
+                        now = readWriteLock.releaseReadLock();
+                        if (sharedCount(now) == 0)
+                        {
+                            
+                        }
+                    }
+                }
+            }
         }
         else
         {
@@ -28,4 +103,26 @@ public class ReadWriteExecutor implements EventHandlerExecutor
         }
     }
     
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private void _handle(EventContext<?> eventContext, EventBus eventBus)
+    {
+        try
+        {
+            EventHandler<?, ?>[] handlers = eventContext.combinationHandlers();
+            Object trans = eventContext.getEventData();
+            for (EventHandler each : handlers)
+            {
+                trans = each.handle(trans, eventBus);
+            }
+            eventContext.setResult(trans);
+        }
+        catch (Throwable e)
+        {
+            eventContext.setThrowable(e);
+        }
+        finally
+        {
+            eventContext.signal();
+        }
+    }
 }
